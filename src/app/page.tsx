497fdba06259d916680d31e5f5294818d4e5578d
Danlela <117685047+Danlela@users.noreply.github.com>
2023-06-27 14:03:30 -0400

    first_commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,10 @@
 "use client"
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import BasicCard from '@/components/molecule/cardform/card'
+
+export default function Home() {
+  return (<BasicCard/>
+  )
+}
