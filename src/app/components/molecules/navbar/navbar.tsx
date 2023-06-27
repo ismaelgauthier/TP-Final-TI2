040477a52ac85afa03e9aa3b3f39cea16a47f172
RaphDou <raphael120701@gmail.com>
2023-06-27 14:52:09 -0400

    Navbar component

--- src/app/components/molecules/navbar/navbar.tsx
+++ src/app/components/molecules/navbar/navbar.tsx
@@ -1,32 +1,49 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
 
-export default function ButtonAppBar() {
+import Sidebar from '../sidebar/sidebar';
+
+function Navbar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleToggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+    <AppBar position="static" sx={{ backgroundColor: '#FFA31A' }}>
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleToggleSidebar}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box sx={{ flexGrow: 1, textAlign: 'center', cursor: 'pointer' }}>
+          <Typography variant="h6" component="div">
+            <Box component="span" sx={{ color: 'white' }}>
+              MyVendorHub
+            </Box>
           </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+        </Box>
+      </Toolbar>
+      <Drawer
+        anchor="left"
+        open={isSidebarOpen}
+        onClose={() => setIsSidebarOpen(false)}
+      >
+        <Sidebar />
+      </Drawer>
+    </AppBar>
   );
 }
+
+export default Navbar;
--- src/app/components/molecules/sidebar/sidebar.tsx
+++ src/app/components/molecules/sidebar/sidebar.tsx
@@ -1,37 +1,34 @@
 import React from 'react';
-import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import HomeIcon from '@mui/icons-material/Home';
 import CategoryIcon from '@mui/icons-material/Category';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Hidden from '@mui/material/Hidden';
+import { Drawer } from '@mui/material';
 
-const Sidebar = () => {
+function Sidebar() {
   return (
-    <Hidden mdDown>
-      <Drawer variant="permanent" anchor="left">
-        <List>
-          <ListItem>
-            <ListItemText primary="Page" />
-          </ListItem>
-          <ListItem button>
-            <HomeIcon />
-            <ListItemText primary="Home" />
-          </ListItem>
-          <ListItem button>
-            <CategoryIcon />
-            <ListItemText primary="Categories" />
-          </ListItem>
-          <ListItem button>
-            <ShoppingCartIcon />
-            <ListItemText primary="Products" />
-          </ListItem>
-        </List>
-      </Drawer>
-    </Hidden>
+    <Drawer variant="permanent" anchor="left">
+      <List>
+        <ListItem>
+          <ListItemText primary="Page" />
+        </ListItem>
+        <ListItem button>
+          <HomeIcon />
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem button>
+          <CategoryIcon />
+          <ListItemText primary="Categories" />
+        </ListItem>
+        <ListItem button>
+          <ShoppingCartIcon />
+          <ListItemText primary="Products" />
+        </ListItem>
+      </List>
+    </Drawer>
   );
-};
+}
 
 export default Sidebar;
