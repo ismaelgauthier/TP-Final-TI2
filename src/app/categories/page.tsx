790d5cf0cda3251ac5d3411dc87b6dd07cfffa1d
RaphDou <raphael120701@gmail.com>
2023-07-08 12:53:02 -0400

    category page 99% done
    category page is functionnal, all that needs to be done is to add some  touch to make it look all pretty and that's it!

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { DataGrid, GridColDef, GridCellParams } from '@mui/x-data-grid';
-import { Card, CardHeader, CardContent } from '@mui/material';
+import { Card, CardHeader, CardContent, Box, IconButton } from '@mui/material';
+import { Settings } from '@mui/icons-material';
 import Link from 'next/link';
+import NewButton from '@/components/molecules/create_button/create_button';
 
 interface Category {
-  id: string; // Ajout du champ 'id'
+  _id: string;
   name: string;
 }
 
@@ -15,11 +17,14 @@ const columns: GridColDef[] = [
     headerName: 'Categories',
     width: 200,
     renderCell: (params: GridCellParams) => (
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Link href={`/categories/update/${params.id}`}>
-          <a>{params.value}</a>
-        </Link>
-      </div>
+      <Link href={`/categories/${params.row._id}`} passHref>
+        <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+          {params.value}
+          <IconButton disabled>
+            <Settings />
+          </IconButton>
+        </div>
+      </Link>
     ),
   },
 ];
@@ -32,12 +37,7 @@ const CategoriesList = () => {
       try {
         const response = await fetch('https://apiraphaeldoucet.onrender.com/categories');
         const data = await response.json();
-        // Ajout de l'ID unique à chaque catégorie
-        const categoriesWithId = data.categories.map((category: Category, index: number) => ({
-          ...category,
-          id: String(index + 1),
-        }));
-        setCategories(categoriesWithId);
+        setCategories(data.categories);
       } catch (error) {
         console.error(error);
       }
@@ -46,20 +46,36 @@ const CategoriesList = () => {
     fetchCategories();
   }, []);
 
+  const rows = categories.map((category, index) => ({
+    id: `category-${index}`,
+    name: category.name,
+    _id: category._id,
+  }));
+
   return (
-    <Card>
-      <CardHeader title="Categories" />
-      <CardContent>
-        <div style={{ height: 400, width: '100%' }}>
-          <DataGrid
-            rows={categories}
-            columns={columns}
-            disableRowSelectionOnClick
-            getRowId={(row) => row.id} // Utilisation de l'ID comme identifiant de ligne
-          />
-        </div>
-      </CardContent>
-    </Card>
+    <Box mt={4}>
+      <Card>
+        <CardHeader
+          title="Categories"
+          action={
+            <Box display="flex" alignItems="center">
+              <NewButton href="/createCategory" />
+            </Box>
+          }
+        />
+        <CardContent>
+          <div style={{ width: '100%' }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              disableRowSelectionOnClick
+              getRowId={(row) => row.id}
+              hideFooterPagination
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
